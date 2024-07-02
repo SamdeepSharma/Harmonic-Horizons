bf655142c9739a3db68a23a8284bdd82f02449f5
Samdeep Sharma <sharmasamdeep1@gmail.com>
2024-07-02 17:10:25 +0530

    courses and contact sections made

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,12 @@
+import Courses from '@/components/Courses'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Courses/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/Contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      Contact team Harmonic Horizons
+    </div>
+  )
+}
+
+export default Contact
